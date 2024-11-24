9a5408770531200b0e8e14c3cabc0d654a21ca8f
yhutter <hutteryannick@gmail.com>
2024-11-24 12:07:32 +0100

    Add sokol library

--- build.zig
+++ build.zig
@@ -4,12 +4,16 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const dep_sokol = b.dependency("sokol", .{ .target = target, .optimize = optimize });
+
     const exe = b.addExecutable(.{
         .name = "zig-game-of-life",
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
+
+    exe.root_module.addImport("sokol", dep_sokol.module("sokol"));
 
     b.installArtifact(exe);
 
--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,29 @@
 const std = @import("std");
+const sokol = @import("sokol");
+const slog = sokol.log;
+const sg = sokol.gfx;
+const sapp = sokol.app;
+const sglue = sokol.glue;
+const print = std.debug.print;
+
+var pass_action: sg.PassAction = .{};
+
+export fn init() void {
+    sg.setup(.{ .environment = sglue.environment(), .logger = .{ .func = slog.func } });
+    pass_action.colors[0] = .{ .load_action = .CLEAR, .clear_value = .{ .r = 1, .g = 1, .b = 0, .a = 1 } };
+    print("Backend: {}\n", .{sg.queryBackend()});
+}
+
+export fn frame() void {
+    sg.beginPass(.{ .action = pass_action, .swapchain = sglue.swapchain() });
+    sg.endPass();
+    sg.commit();
+}
+
+export fn cleanup() void {
+    sg.shutdown();
+}
 
 pub fn main() void {
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    sapp.run(.{ .init_cb = init, .frame_cb = frame, .cleanup_cb = cleanup, .width = 640, .height = 480, .icon = .{ .sokol_default = true }, .window_title = "Zig Game of Life", .logger = .{ .func = slog.func }, .win32_console_attach = true });
 }
