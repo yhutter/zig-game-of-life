490f2c2f78e4a566026b29b8b68b1642481f595b
yhutter <hutteryannick@gmail.com>
2024-11-24 17:55:32 +0100

    Implement drawing cell state

--- src/main.zig
+++ src/main.zig
@@ -13,7 +13,20 @@ const native_endian = @import("builtin").target.cpu.arch.endian();
 
 const window_width = 1280;
 const window_height = 960;
-const background_color = makeColorRGBA8(0x181818ff);
+const cell_size: u32 = 32;
+const num_cells_x: usize = window_width / cell_size;
+const num_cells_y: usize = window_height / cell_size;
+const num_total_cells = num_cells_x * num_cells_y;
+
+const background_color = makeColorRGBA8(0x96a6c8ff);
+const foreground_color = makeColorRGBA8(0x181818ff);
+const cell_alive_color = foreground_color; 
+const cell_dead_color = background_color; 
+
+const CellState = enum {
+    dead,
+    alive
+};
 
 
 const state = struct {
@@ -21,6 +34,7 @@ const state = struct {
     var pip: sg.Pipeline = .{};
     var bind: sg.Bindings = .{};
     var pixel_buffer = std.mem.zeroes([window_width * window_height]u32);
+    var cells: [num_cells_x * num_cells_y]CellState = undefined; 
 };
 
 
@@ -85,6 +99,11 @@ export fn init() void {
         .load_action = .CLEAR,
         .clear_value = .{ .r = 0, .g = 0, .b = 0, .a = 1}
     };
+
+    // Set initial cell state
+    @memset(&state.cells, .dead);
+
+    state.cells[0] = .alive;
 }
 
 fn makeColorRGBA8(color: u32) u32 {
@@ -103,13 +122,45 @@ fn makeColorRGBA8(color: u32) u32 {
     }
 }
 
+inline fn drawPixel(x: usize, y: usize, color: u32) void {
+    const outOfBounds = x < 0 or x > window_width or y < 0 or y > window_height;
+    if (!outOfBounds) {
+        state.pixel_buffer[(y * window_width) + x] = color;
+    }
+}
+
 
 fn clearColorBuffer(color: u32) void {
     @memset(&state.pixel_buffer, color);
 }
 
+fn drawRectangle(x: usize, y: usize, w: usize, h: usize, color: u32) void {
+    for (0..h) |i| {
+        for (0..w) |j| {
+            const curr_x = j + x;
+            const curr_y = i + y;
+            drawPixel(curr_x, curr_y, color);
+        }
+    }
+}
+
+fn drawCellStates() void {
+    for (0.., state.cells) |i, cell_state| {
+        // Convert index to x and y position
+        const x = i % num_cells_x;
+        const y = i / num_cells_x;
+
+        const color = switch(cell_state) {
+            .dead => cell_dead_color,
+            .alive => cell_alive_color
+        };
+        drawRectangle(x * cell_size, y * cell_size, cell_size, cell_size, color);
+    }
+}
+
 export fn frame() void {
     clearColorBuffer(background_color);
+    drawCellStates();
 
     var image_data: sg.ImageData = .{};
     image_data.subimage[0][0] = sg.asRange(&state.pixel_buffer);
